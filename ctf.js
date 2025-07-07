e5bbe3d905adf4a29c53e215638e352f0e53b28c
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2025-07-07 22:43:56 +0530

    Add files via upload

---
+++ ctf.js
@@ -0,0 +1,12 @@
+const text = "In the shadows of cyberspace, secrets are buried deep—waiting for those bold enough to uncover them. “Capture the Flag” isn’t just a game; it’s a digital manhunt. Each flag you find peels back another layer of the unknown, drawing you deeper into encrypted puzzles, vulnerable systems, and hidden trails. Do you have the instincts of a hacker and the mind of a detective? Step into the arena, follow the whispers in the code, and prove your skills where every second counts. The flag is out there... can you capture it?"; 
+let i = 0;
+
+function typeEffect() {
+  if (i < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 20); 
+  }
+}
+
+window.onload = typeEffect;
