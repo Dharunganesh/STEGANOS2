8234c5fea6e8e2ce3cd045553d6c37cc320bf338
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2025-08-16 23:52:36 +0530

    Update bgmi.js

--- bgmi.js
+++ bgmi.js
@@ -1,7 +1,7 @@
 const text="The battleground awaits. Bullets, strategy, and survival instincts collide in this digital warzone. Squad up, drop in, and fight for dominance — every kill, every placement, every heartbeat counts. Will you crumble under pressure or rise to claim the ultimate chicken dinner? The zone is shrinking… are you ready to conquer it?
 The battleground decides the brave.
 Shots fired, squads clashing, and strategies unfolding — every move shapes victory. Survival is the only law.
-"
+";
 let index=0
 
 function func(){
@@ -15,3 +15,4 @@ func()
 
 
 window.onload=typeEffect
+
